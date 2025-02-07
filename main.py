11b7a25fcc649d5cec5bf26ba4c15e5fcc19fb3f
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-02-06 19:55:21 -0500

    add router

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from routers import artists
 
 app = FastAPI()
 
+app.include_router(artists.router, prefix="/artists", tags=["artists"])
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to ArtBase:Two"}
+    return {"message": "Welcome to Golebiewski Artworks Database API"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
---
+++ routers/artists.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("names")
+async def get_artist_names():
+    return {"names: " : "Artist Names Here..."}
